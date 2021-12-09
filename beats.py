76c77ff75cf3b64535949a1fcca4ecfb4feca757
IIJJ22GG <72188231+IIJJ22GG@users.noreply.github.com>
2021-12-08 19:24:10 -0800

    The critical fix
    Bot will no longer spam the API, will display the time correctly, and will be more accurate.

--- beats.py
+++ beats.py
@@ -3,14 +3,23 @@ from discord.ext import tasks
 from math import floor
 
 bot = discord.Client()
+oldTime = 0
 
 @tasks.loop(seconds=2)
 async def status():
     global bot
-    bTime = str(floor(((int(time.strftime("%M", time.gmtime())) * 60) + ((int(time.strftime("%H", time.gmtime())) + 1) * 3600)) / 86.4))
-    try:
-        if bTime > oldTime: await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="@" + str(bTime)))
-    except: await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="@" + str(bTime)))
+    global oldTime
+    hour = int(time.strftime("%H", time.gmtime()))
+    minute = int(time.strftime("%M", time.gmtime()))
+    second = int(time.strftime("%S", time.gmtime()))
+    bTime = floor(((second + minute * 60) + (hour + 1) * 3600) / 86.4)
+    if bTime < 100:
+        if bTime < 10: bTime = "00" + str(bTime)
+        else: bTime = "0" + str(bTime)
+    if int(bTime) == 1000:
+        bTime = "000"
+        oldTime = -1
+    if int(bTime) > int(oldTime): await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="@" + str(bTime)))
     oldTime = bTime
 
 @bot.event
