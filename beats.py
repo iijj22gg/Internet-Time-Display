d1795389490b7f4083dff1cfac66ecf0e25888bf
iijj22gg <iijj22ggtheperson@gmail.com>
2025-03-29 17:32:38 -0500

    Big refactor

--- beats.py
+++ beats.py
@@ -1,32 +1,22 @@
-import time, discord
-from discord.ext import tasks
-from math import floor
+import time
+import discord
+from datetime import datetime, timedelta, timezone
 
 client = discord.Client(intents=discord.Intents.none())
-oldTime = 0
 
-@tasks.loop(seconds=2)
-async def status():
-    global client
-    global oldTime
-    hour = int(time.strftime("%H", time.gmtime()))
-    minute = int(time.strftime("%M", time.gmtime()))
-    second = int(time.strftime("%S", time.gmtime()))
-    bTime = floor(((second + minute * 60) + (hour + 1) * 3600) / 86.4)
-    if bTime > 1000: bTime = bTime - 1000
-    if bTime < 100:
-        if bTime < 10: bTime = "00" + str(bTime)
-        else: bTime = "0" + str(bTime)
-    if int(bTime) == 1000:
-        bTime = "000"
-        oldTime = -1
-    if int(bTime) > int(oldTime): await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="@" + str(bTime)))
-    oldTime = bTime
+async def update_status():
+    
+    while True:
+        now = datetime.now(timezone(timedelta(hours=1)))
+        beats = ((now.hour * 3600 + now.minute * 60 + now.second) / 86.4) % 1000
+        await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name=f"@{beats:03.0f}"))
+        
+        next_beat_seconds = 86.4 - (now.second + now.microsecond / 1_000_000) % 86.4
+        await discord.utils.sleep_until(datetime.utcnow() + timedelta(seconds=next_beat_seconds))
 
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
-    try: await status.start()
-    except: pass
+    client.loop.create_task(update_status())
 
 client.run('TOKEN')
